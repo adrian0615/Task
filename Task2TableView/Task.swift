72601be988cd9eb36505b7e5da24f0be64df0994
Adrian McDaniel <adriandmcdaniel@gmail.com>
2017-02-01 19:01:24 -0500

    First Commit

---
+++ Task2TableView/Task.swift
@@ -0,0 +1,19 @@
+//
+//  Task.swift
+//  Task2TableView
+//
+//  Created by Adrian McDaniel on 2/1/17.
+//  Copyright © 2017 dssafsfsd. All rights reserved.
+//
+
+import Foundation
+
+
+class Task {
+    
+    let title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+}
